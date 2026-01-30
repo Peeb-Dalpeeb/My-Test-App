329f301bdd09a7f6a31b574e3d1701d546534be9
Scott Dalpias <instructionalmotion@gmail.com>
2026-01-29 20:12:11 -0600

    Project set up and ready to branch

---
+++ src/components/Profile.tsx
@@ -0,0 +1,16 @@
+
+interface ProfileProps {
+  imageUrl: string;
+  name: string;
+  occupation: string;
+}
+
+export function Profile({ imageUrl, name, occupation }: ProfileProps) {
+    return (
+        <section className="flex flex-col items-center p-10">
+            <img className="w-32 h-32 rounded-full object-cover" src={imageUrl} alt={name}  />
+            <h2 className="text-lg font-medium mt-4">{name}</h2>
+            <p className="text-base font-light text-gray-600 mt-2">{occupation}</p>
+        </section>
+    );
+}
