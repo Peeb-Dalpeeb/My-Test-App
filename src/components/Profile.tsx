252e403b8d5cf377861d15aaa07f8db1a8c8fb5c
Scott Dalpias <instructionalmotion@gmail.com>
2026-01-31 23:16:25 -0600

    Refactor Profile component styles for improved layout and color consistency

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -7,10 +7,10 @@ interface ProfileProps {
 
 export function Profile({ imageUrl, name, occupation }: ProfileProps) {
     return (
-        <section className="flex flex-col items-center p-10">
+        <section className="flex flex-col items-center pb-8">
             <img className="w-32 h-32 rounded-full object-cover" src={imageUrl} alt={name}  />
             <h2 className="text-lg font-medium mt-4">{name}</h2>
-            <p className="text-base font-light text-gray-600 mt-2">{occupation}</p>
+            <p className="text-base font-light text-muted mt-2">{occupation}</p>
         </section>
     );
 }
